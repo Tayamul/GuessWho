27f99074b43b31da3cb94dcac7d13ac3bc8aa194
Tayamul <rai_tayamul@hotmail.com>
2024-07-15 12:05:14 +0100

    create a Female class

---
+++ src/main/scala/Female.scala
@@ -0,0 +1,4 @@
+case class Female (name: String, glasses: Boolean, hairColour: String, hat: Boolean, eyeColour: String) extends Character {
+  val gender: String = "Female"
+  val hasHair: Boolean = true
+}
