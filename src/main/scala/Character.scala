6914a364706ab2f51ce4387bbbc162678d941645
Tayamul <rai_tayamul@hotmail.com>
2024-07-15 12:03:10 +0100

    create a Character class

---
+++ src/main/scala/Character.scala
@@ -0,0 +1,9 @@
+abstract class Character {
+  val name: String
+  val hasHair: Boolean
+  val hairColour: String
+  val glasses: Boolean
+  val hat: Boolean
+  val eyeColour: String
+  val gender: String
+}
--- src/main/scala/test.md
+++
@@ -1,5 +0,0 @@
-Here is some test text to make sure it is not an issue
-Another line of text
-another line of text again 
-another text on a new branch
-here is the final test
