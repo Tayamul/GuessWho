24a57cd3d346a46182e6cccdbbc2127bc9aba842
Tayamul <rai_tayamul@hotmail.com>
2024-07-15 12:04:16 +0100

    create a BladMan class

---
+++ src/main/scala/BaldMan.scala
@@ -0,0 +1,4 @@
+case class BaldMan (name: String, glasses: Boolean, facialHair: Boolean, hat: Boolean, eyeColour: String) extends Male {
+  val hasHair: Boolean = false
+  val hairColour: String = null
+}
