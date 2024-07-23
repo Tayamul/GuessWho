9424614318a9270e2a6d820b79afb5f3f57a2d8e
Tayamul <rai_tayamul@hotmail.com>
2024-07-23 13:04:42 +0100

    refactor the matchCharacteristics method to filter and pattern match the Male case class

--- src/main/scala/UtilityFunctions.scala
+++ src/main/scala/UtilityFunctions.scala
@@ -12,36 +12,25 @@ object UtilityFunctions {
   // pattern matching characteristics you choose against the characters in the map
   def matchCharacteristics(individualsMap: Map[Int, Character], feature: String, value: Any): Map[Int, Character] = {
     individualsMap.filter {
-      case (_, BaldMan(name, glasses, facialHair, hat, eyeColour)) => feature match {
+      case (_, Male(name, hasHair, glasses, facialHair, hairColour, hat, eyeColour)) => feature match {
         case "name" => name == value
-        case "glasses" => glasses == value
-        case "facialhair" => facialHair == value
-        case "hat" => hat == value
-        case "eyecolour" => eyeColour == value
-        case "hair" => false == value
-        case "haircolour" => true == value
-        case "gender" => "male" == value
-        case _ => true
-      }
-      case (_, HairMan(name, glasses, facialHair, hairColour, hat, eyeColour)) => feature match {
-        case "name" => name == value
+        case "hashair" => hasHair == value
         case "glasses" => glasses == value
         case "facialhair" => facialHair == value
         case "haircolour" => hairColour == value
         case "hat" => hat == value
         case "eyecolour" => eyeColour == value
-        case "hair" => true == value
         case "gender" => "male" == value
         case _ => true
       }
       case (_, Female(name, glasses, hairColour, hat, eyeColour)) => feature match {
         case "name" => name == value
+        case "hashair" => true == value
         case "glasses" => glasses == value
-        case "haircolour" => hairColour == value
         case "facialhair" => false == value
+        case "haircolour" => hairColour == value
         case "hat" => hat == value
         case "eyecolour" => eyeColour == value
-        case "hair" => true == value
         case "gender" => "female" == value
         case _ => true
       }
